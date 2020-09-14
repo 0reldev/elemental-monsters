0c00502716357af9b86214b13ea2d8639085bd5a
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-09-14 16:55:43 +0200

    Code cleaning

--- Arena.java
+++ Arena.java
@@ -5,59 +5,66 @@ class Arena {
     public static Scanner input = new Scanner(System.in);
 
     public static void counterAttack(Monster ennemy, Monster champion) {
+
         if (ennemy.getLife() > 0 ) {
+
             ennemy.attack(champion);
         }
     }
 
     public static void fight(Monster champion, Monster ennemy) {        
             
-            String answer = "";
-            boolean stop = false;
+        String answer = "";
+        boolean stop = false;
+        while (answer != "a" || answer != "h") {
 
-            while (answer != "a" || answer != "h") {
-                if (champion.getLife() > 0 && ennemy.getLife() > 0){
-                    System.out.println("What is your next action? Type 'a' for attacking, 'h' for healing.");
-                    answer = input.nextLine();
-                    if (answer.equals("a")) { 
-                        champion.attack(ennemy);
-                        answer = "";
-                        counterAttack(ennemy, champion);
+            if (champion.getLife() > 0 && ennemy.getLife() > 0) {
 
-                    } else if (answer.equals("h")){
-                        champion.heal();
-                        answer = "";
-                        counterAttack(ennemy, champion);
-                    } else {
-                        System.out.println( "Invalid answer!");
-                    }
+                System.out.println("What is your next action? Type 'a' for attacking, 'h' for healing.");
+                answer = input.nextLine();
+                if (answer.equals("a")) {
+
+                    champion.attack(ennemy);
+                    answer = "";
+                    counterAttack(ennemy, champion);
+                } else if (answer.equals("h")) {
+
+                    champion.heal();
+                    answer = "";
+                    counterAttack(ennemy, champion);
+                } else {
+
+                    System.out.println( "Invalid answer!");
                 }
-            
-                else {
-                    if (champion.getLife()<= 0 && !stop) {
-                        System.out.println("GAME OVER!");
-                        stop = true;
-                        break;
-                                                
-                    } else {
-                        System.out.println(champion.getName() + " wins! ");
-                        break;
-                    }                          
+            } else {
+                if (champion.getLife()<= 0 && !stop) {
+
+                    System.out.println("GAME OVER!");
+                    stop = true;
+                    break;
+                } else {
+
+                    System.out.println(champion.getName() + " wins! ");
+                    break;
                 }
-            }                   
+            }
+        }
     }
 
     private static int round(Monster champion, Monster opponent, int round) {
 
         if (champion.getLife() > 0) {
+
             System.out.println("\nROUND " + round + "\n");
             champion.status();
             opponent.status();
             while (opponent.getLife() > 0 && champion.getLife() > 0) {
+
                 fight(champion, opponent);
             }
         }
         if (champion.getLife() > 0) {
+
             return 1;
         }
         return 0;
@@ -65,7 +72,6 @@ class Arena {
 
     public static void main (String[] args) {
 
-        // creation of new instances
         Monster champion = new Monster("Champion", 40.0);
         Monster ennemy1 = new Monster("Ennemy 1"); 
         Monster ennemy2 = new Monster("Ennemy 2");
@@ -78,5 +84,4 @@ class Arena {
         
         System.out.println("Victorious rounds: " + nbVictories);
     }
-
 }
--- Monster.java
+++ Monster.java
@@ -1,13 +1,10 @@
 import java.util.Random;
-// import java.util.Scanner;
-
 
 class Monster {
 
     Random r = new Random();
     String[] elements = {"fire", "water", "air"};
 
-    // attributes
     private String name;
     private int strength;   // de 0 à 9
     private int intelligence;   // de 0 à 9
@@ -15,9 +12,8 @@ class Monster {
     private double initialLife;
     private String element; // "fire", "water", "air"
 
+    public Monster(String name, double life) {
 
-    // constructors
-    public Monster(String name, double life){
         this.name = name;
         this.strength = r.nextInt(9);
         this.intelligence = r.nextInt(9);
@@ -26,7 +22,8 @@ class Monster {
         this.element = elements[r.nextInt(3)];
     }
 
-    public Monster(String name){
+    public Monster(String name) {
+
         this.name = name;
         this.strength = r.nextInt(9);
         this.intelligence = r.nextInt(9);
@@ -35,8 +32,6 @@ class Monster {
         this.element = elements[r.nextInt(3)];
     }
 
-
-    // setters
     public void setName(String name) {
         this.name = name;
     }
@@ -60,8 +55,7 @@ class Monster {
     public void setElement(String element) {
         this.element = element;
     }
-    
-    // getters
+
     public String getName() {
         return this.name;
     }
@@ -78,7 +72,6 @@ class Monster {
         return this.initialLife;
     }
 
-
     public int getIntelligence() {
         return this.intelligence;
     }
@@ -86,48 +79,49 @@ class Monster {
     public String getElement() {
         return this.element;
     }
-    
 
-    // instance method
-    public void attack(Monster ennemyMonster) {      
+    public void attack(Monster ennemyMonster) {
+
         int attack = getStrength() + 1;           
         double efficiency = 1.0;
         if (this.element == "fire" && ennemyMonster.element == "air" 
             || this.element == "water" && ennemyMonster.element == "fire"
             || this.element == "air" && ennemyMonster.element == "water") {
+
                 efficiency = 2.0 ;
         } else if ( this.element == "air" && ennemyMonster.element == "fire" 
-        || this.element == "fire" && ennemyMonster.element == "water"
-        || this.element == "water" && ennemyMonster.element == "air") {
+            || this.element == "fire" && ennemyMonster.element == "water"
+            || this.element == "water" && ennemyMonster.element == "air") {
+
                 efficiency = 0.5 ;
         }
         ennemyMonster.setLife( ennemyMonster.getLife() - attack * efficiency );
-
         if (ennemyMonster.getLife() > 0) {
+
             System.out.println(this.getName() + " attacks " + ennemyMonster.getName() + ".\n" + ennemyMonster.getName() + " has " + ennemyMonster.getLife() + " points remaining.\n");
         } else {
+
             System.out.println(this.getName() + " attacks " + ennemyMonster.getName() + ".\n" + ennemyMonster.getName() +" is KO!\n") ;          
         }                                
     }
 
-    public void heal() {        
+    public void heal() {
+
         int heal = getIntelligence() + 1;
         if (this.getLife() < this.getInitialLife()) {
+
             this.setLife(this.getLife() + heal) ;
-            if (this.getLife() > this.getInitialLife()){
+            if (this.getLife() > this.getInitialLife()) {
+
                 this.setLife(this.getInitialLife());
             }
             System.out.println(this.getName() + " heals. It now has " + this.getLife() + " of life.\n");
-            // System.out.printf("%s heals. It now has %.1f of life.\n\n", this.getName(), this.getLife());
         } else {
+
             System.out.println(this.getName() + "'s life is at maximum already, so " + this.getName() + " can't heal.\n" );
         }
     }
 
-
-
-
-    // display the stats of a monster
     public void status() {
         System.out.println(this.getName());
         System.out.println("Strength: " + this.getStrength());
@@ -135,7 +129,4 @@ class Monster {
         System.out.println("Life: " + this.getLife());
         System.out.println("Element: " + this.getElement() + "\n");
     }
-
-
-
 }
